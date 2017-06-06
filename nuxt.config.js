0292de277958de2312ed18f0a57263f3aa7181b3
Roberto <roberto.butti@gmail.com>
2017-06-06 23:18:14 +0200

    added loading

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,12 +1,17 @@
 const { join } = require('path')
 
 module.exports = {
-  css: [
-    //'hover.css/css/hover-min.css',
-    //'bulma/bulma.sass',
-    join(__dirname, 'css/main.css')
-  ],
-  build: {
-    extractCSS: true
-  }
+	loading: 'components/loading.vue',
+	css: [
+		//'hover.css/css/hover-min.css',
+		//'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+		//'https://cdnjs.cloudflare.com/ajax/libs/bulma/0.4.2/css/bulma.min.css',
+		//'bulma/bulma.css',
+		//{ src: 'bulma', lang: 'sass' },
+		join(__dirname, 'css/bulma.css'),
+		join(__dirname, 'css/main.css')
+	],
+	build: {
+		extractCSS: true
+	}
 }
