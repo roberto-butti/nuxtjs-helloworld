00963b85b67435ef0ee456351f63c316850e1379
Roberto <roberto.butti@gmail.com>
2017-06-06 20:03:44 +0200

    playing with asyncData

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,28 @@
 <template>
 <div>
-    <p>Hi from XXX</p>
+	<h1 class="red">Hello {{ name }}!</h1>
+    <p>Hi from me, {{ datex }}</p>
+	<p>{{ inc }}</p>
+	<p>{{ myreq }}</p>
     <nuxt-link to="/">Home page</nuxt-link>
   </div>
  </template>
+
+<script>
+var i=0;
+export default {
+  asyncData ({ req }) {
+  	var retVal={ name: 'worlds' };
+  	retVal["datex"]="a"+Date.now();
+  	retVal["inc"]=i++;
+  	retVal["myreq"]= req ? "SERVER" : "none";
+    return retVal;
+  }
+}
+</script>
+
+<style>
+.red {
+  color: red;
+}
+</style>
