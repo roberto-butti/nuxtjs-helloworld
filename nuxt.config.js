128336670e972aa534e1db5473a4ca299ee54c1b
Roberto <roberto.butti@gmail.com>
2017-06-06 20:18:58 +0200

    added custom CSS

---
+++ nuxt.config.js
@@ -0,0 +1,12 @@
+const { join } = require('path')
+
+module.exports = {
+  css: [
+    //'hover.css/css/hover-min.css',
+    //'bulma/bulma.sass',
+    join(__dirname, 'css/main.css')
+  ],
+  build: {
+    extractCSS: true
+  }
+}
